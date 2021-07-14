df3b7d40a198e90765a639a27b7abf242b16e95b
abenezer2008 <abenezersintayehu22@gmail.com>
2021-07-13 22:19:37 -0500

    added alert Strategy

--- src/miu/edu/cs/cs525/final_project/framework/observer/EmailSender.java
+++ src/miu/edu/cs/cs525/final_project/framework/observer/EmailSender.java
@@ -1,6 +1,10 @@
 package miu.edu.cs.cs525.final_project.framework.observer;
 
 import miu.edu.cs.cs525.final_project.framework.model.Account;
+import miu.edu.cs.cs525.final_project.framework.model.AccountEntry;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class EmailSender implements Observer {
     private volatile static EmailSender Instance;
@@ -8,7 +12,15 @@ public class EmailSender implements Observer {
 
     @Override
     public void update(Account account) {
+        Collection<AccountEntry> accountEntries = account.getAccountEntry();
+        double alertLimit = account.getAlertStrategy().alertAmount();
+        int entry_length = accountEntries.size();
+        double lastAmount = accountEntries.stream().collect(Collectors.toList()).get(entry_length).getAmount();
 
+        if (lastAmount > alertLimit) {
+           String  email = account.getCustomer().getEmail();
+//            this.notifyAllObservers(account);
+        }
     }
 
     public static EmailSender getInstance(){
